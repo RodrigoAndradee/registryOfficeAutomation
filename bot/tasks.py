481766fca148b650e196b3d378772493fa8b6cd3
Rodrigo Andrade <rodrigomachado161@gmail.com>
2025-05-18 21:28:09 -0300

    updating task to avoid multiple login

--- bot/tasks.py
+++ bot/tasks.py
@@ -15,17 +15,18 @@ def navigate_through_menu(page, fields):
     page.hover(fields["registry_office"])
     page.click(fields["declare"])
 
-def fill_form_content(page, fields):
-    page.fill(fields["code_act"], "	4127-7")
+def fill_form_content(page, fields, data):
+    page.fill(fields["code_act"], data["code"])
 
 @shared_task(bind=True)
-def execute_form(self, history_id):
+def execute_form(self, data):
+    history_instance = None
     
     json_file_path = os.path.join(os.path.dirname(__file__), 'static', 'data', 'form_fields.json')
     with open(json_file_path, 'r', encoding='utf-8') as file:
         form_fields = json.load(file)
-        # print("JSON CONTENT", json.dumps(login_fields, indent=4, ensure_ascii=False))
-
+    
+    # Trying to login
     try:
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=False)
@@ -34,13 +35,25 @@ def execute_form(self, history_id):
 
             fill_login_form(page, form_fields)
             navigate_through_menu(page, form_fields)
-            fill_form_content(page, form_fields)
+
+            # Runs the automation on each position of the array
+            for item in data:
+                try: 
+                    history_instance = AutomationHistory.objects.create(
+                        code = item["code"],
+                        quantity = item["quantity"],
+                        type = item["type"]
+                    )
+                    fill_form_content(page, form_fields, item)
+
+                    # Updating the status on the database
+                    AutomationHistory.objects.filter(id=history_instance.history_id).update(status="SUCCESS")
+                except Exception as form_error:
+                    # Updating the status on the database
+                    AutomationHistory.objects.filter(id=history_instance.history_id).update(status="ERROR")
+        
+            connection.close()
             browser.close()
-
-        AutomationHistory.objects.filter(id=history_id).update(status="SUCCESS")
-        connection.close()
-
+            
     except Exception as e:
-        AutomationHistory.objects.filter(id=history_id).update(status="ERROR")
-        connection.close()
         raise e
--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,4 @@
+import os
 import json
 from django.contrib import messages
 from django.views import View
@@ -15,35 +16,33 @@ class ListHistory(View):
     
     def post(self, request):
         histories = AutomationHistory.objects.all()
-        data = None 
+        json_content = None 
         form = UploadJSONForm(request.POST, request.FILES)
+
         if form.is_valid():
             uploaded_file = form.cleaned_data['file']
+
             try:
-                data = json.load(uploaded_file)
-                print("JSON CONTENT", json.dumps(data, indent=4, ensure_ascii=False))
+                json_content = json.load(uploaded_file)
+                print("JSON CONTENT", json.dumps(json_content, indent=4, ensure_ascii=False))
             except json.JSONDecodeError:
                 form.add_error('file', 'Arquivo JSON inválido.')
                 return render(request, 'bot/automation_history.html', {'form': form, 'histories': histories})
 
-            is_valid, error_msg = self.validate_json_fields(data)
+            is_valid, error_msg = self.validate_json_fields(json_content)
 
             if not is_valid:
                 messages.error(request, f"Erro de validação: {error_msg}", extra_tags="alert-danger")
                 return redirect("history")
             
             form = UploadJSONForm()
+            json_chunks = self.split_chunks(json_content["automation_data"])
+
+            print("CHUNKS CONTENT", json.dumps(json_chunks, indent=4, ensure_ascii=False))
             
             # Run automation
-            for item in data["automation_data"]:
-                instance = AutomationHistory.objects.create(
-                    code = item["code"],
-                    quantity = item["quantity"],
-                    type = item["type"]
-                )
-                
-                execute_form.delay(instance.id)
-                # execute_form(instance.id)
+            for chunk in json_chunks:
+                execute_form.delay(chunk)
                 
             messages.success(request, "JSON importado com sucesso!", extra_tags="alert-success")
             return redirect("history")
@@ -73,3 +72,23 @@ class ListHistory(View):
                 return False, f"Campo 'type' no item {i} deve ser uma string."
 
         return True, ""
+    
+    def split_chunks(self, data):
+        # This function breaks the json array in small arrays to match the workers amount
+        workers_amount = os.getenv("WORKERS_AMOUNT")
+
+        if workers_amount <= 1:
+            return [data]
+
+        chunk_size = len(data) // workers_amount
+        remainder = len(data) % workers_amount
+
+        result = []
+        start = 0
+
+        for i in range(workers_amount):
+            end = start + chunk_size + (1 if i < remainder else 0)
+            result.append(data[start:end])
+            start = end
+
+        return result
