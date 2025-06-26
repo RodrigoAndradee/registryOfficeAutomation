bffc20709412072e465677f87408f7762f5853c7
Rodrigo <rodrigomachado161@gmail.com>
2025-06-25 23:29:02 -0300

    Update file editor aplication

--- file_editor/static/js/script.js
+++ file_editor/static/js/script.js
@@ -1,6 +1,16 @@
 tinymce.init({
-    selector: 'textarea#id_your_field', // Replace with your actual textarea ID
-    plugins: 'autoresize', // Example plugin
-    content_style: 'p { margin: 16px 0; }', // Apply custom margin to paragraphs
-    // Add other TinyMCE configurations as needed
+  selector: "textarea#id_your_field", // Replace with your actual textarea ID
+  plugins: "exportpdf", // Example plugin
+  toolbar: "exportpdf",
+
+  exportpdf_token_provider: () => {
+    return fetch("http://localhost:3000/jwt", {
+      // specify your token endpoint
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => response.json());
+  },
+
+  content_style: "p { margin: 16px 0; }", // Apply custom margin to paragraphs
+  // Add other TinyMCE configurations as needed
 });
--- file_editor/views.py
+++ file_editor/views.py
@@ -1,16 +1,44 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from django.http import HttpResponse
+
+from django.template.loader import render_to_string
+
+from weasyprint import HTML
+from .models import Post
 
 from .forms import PostForm
 
 class FileEditor(View):
+    
+    def export_pdf(request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        html_string = render_to_string("file_editor/file_editor.html", { "post": post })
+        html = HTML(string=html_string)
+        
+        pdf_file = html.white_pdf()
+        
+        response = HttpResponse(pdf_file, content_type="application/pdf")
+        response["Content-Disposition"] = f"filename=post_{post.id}.pdf"
+        return response
 
     def post(self, request):
         form = PostForm(request.POST)
-        # if form.is_valid():
-        #     form.save()
-        #     return redirect("success")
-        
+        if form.is_valid():
+            post = form.save()
+            title = form.cleaned_data["title"]
+            
+            print(post)
+            
+            html_string = render_to_string("export_pdf/export_pdf.html", {"post": post})
+            pdf_file = HTML(string=html_string).write_pdf()
+
+            response = HttpResponse(pdf_file, content_type="application/pdf")
+            response["Content-Disposition"] = f"attachment; filename={title}.pdf"
+            return response
+            
+        else :
+            form = PostForm()
         return render(request, "file_editor/file_editor.html", {"form": form})
     
 
